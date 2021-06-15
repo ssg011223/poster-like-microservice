8916b12852cd472a727b99fd936068d1c4e55f08
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-06-15 11:00:24 +0200

    initial commit

---
+++ src/main/java/com/codecool/posterlikemicroservice/PosterLikeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.posterlikemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterLikeMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosterLikeMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/posterlikemicroservice/PosterLikeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.posterlikemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterLikeMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
